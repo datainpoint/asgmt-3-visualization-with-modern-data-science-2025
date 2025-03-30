e9a2a2ccfc301734ab01dfb81d7f6b39984b2f55
yaojenkuo <tonykuoyj@gmail.com>
2025-03-30 20:56:02 +0800

    first commit

---
+++ answers.py
@@ -0,0 +1,69 @@
+answer_01 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_02 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_03 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_04 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_05 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_06 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_07 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_08 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_09 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_10 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
